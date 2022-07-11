5eb21cb65ee0249080bcb38514806a8f21f2e33e
Diego Almeida <diegoalmda@gmail.com>
2022-07-11 14:46:09 -0300

    Todo CRUD finished

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,8 @@ const server = express();
 
 server.use(cors());
 
+// server.use(express.json());
+
 server.use(express.static(path.join(__dirname, '../public')));
 server.use(express.urlencoded({ extended: true }));
 
